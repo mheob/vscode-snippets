32a44d0538c7d32067033aaca5fcd6b42ef3ac1c
Alexander Böhm <3983539+mheob@users.noreply.github.com>
2022-03-13 09:25:25 +0100

    :zap: Speed up snippet generation (#66)
    Also update dependencies

--- scripts/generate-snippet-files.ts
+++ scripts/generate-snippet-files.ts
@@ -1,5 +1,4 @@
-import { mkdir, rm, writeFile } from 'node:fs/promises';
-
+import { mkdir, rm } from 'node:fs/promises';
 import {
   JS_FILE_NAME,
   JS_REACT_FILE_NAME,
@@ -13,27 +12,25 @@ import {
   TS_REACT_SNIPPETS_DIRECTORY,
   TS_SNIPPETS_DIRECTORY,
 } from './constants';
-import { Snippet } from './types';
-import { getSnippetsFromFolder } from './utils';
-
-const writeSnippetFile = async (file: string, snippetObject: Record<string, Snippet>) => {
-  writeFile(`${OUTPUT_DIRECTORY}/${file}`, JSON.stringify({ ...snippetObject }));
-};
+import { getSnippetsFromFolder, writeSnippetFile } from './utils';
 
 const generateSnippetFiles = async () => {
   await rm(OUTPUT_DIRECTORY, { recursive: true, force: true });
   await mkdir(OUTPUT_DIRECTORY);
 
   const mdSnippetsObject = await getSnippetsFromFolder(MD_SNIPPETS_DIRECTORY);
-  const jsSnippetsObject = await getSnippetsFromFolder(JS_SNIPPETS_DIRECTORY);
-  const jsReactSnippetsObject = await getSnippetsFromFolder(JS_REACT_SNIPPETS_DIRECTORY);
-  const tsSnippetsObject = await getSnippetsFromFolder(TS_SNIPPETS_DIRECTORY);
-  const tsReactSnippetsObject = await getSnippetsFromFolder(TS_REACT_SNIPPETS_DIRECTORY);
-
   writeSnippetFile(MD_FILE_NAME, mdSnippetsObject);
+
+  const jsSnippetsObject = await getSnippetsFromFolder(JS_SNIPPETS_DIRECTORY);
   writeSnippetFile(JS_FILE_NAME, jsSnippetsObject);
+
+  const jsReactSnippetsObject = await getSnippetsFromFolder(JS_REACT_SNIPPETS_DIRECTORY);
   writeSnippetFile(JS_REACT_FILE_NAME, { ...jsSnippetsObject, ...jsReactSnippetsObject });
+
+  const tsSnippetsObject = await getSnippetsFromFolder(TS_SNIPPETS_DIRECTORY);
   writeSnippetFile(TS_FILE_NAME, { ...jsSnippetsObject, ...tsSnippetsObject });
+
+  const tsReactSnippetsObject = await getSnippetsFromFolder(TS_REACT_SNIPPETS_DIRECTORY);
   writeSnippetFile(TS_REACT_FILE_NAME, {
     ...jsSnippetsObject,
     ...jsReactSnippetsObject,
