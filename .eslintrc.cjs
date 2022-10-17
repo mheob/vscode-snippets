f128e126fa7556b9d193081b3ac2f749da85aebb
Alexander Böhm <tools@boehm.work>
2022-10-17 08:49:27 +0200

    :arrow_up: Update dependencies and erformat code

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,10 @@
 /** @type {import('eslint').ESLint.ConfigData} */
 module.exports = {
-  root: true,
-  extends: ['@mheob/eslint-config'],
-  rules: {
-    'unicorn/no-array-reduce': 'off',
-    'unicorn/prefer-module': 'off',
-    'unicorn/prefer-object-from-entries': 'off',
-  },
+	root: true,
+	extends: ['@mheob/eslint-config'],
+	rules: {
+		'unicorn/no-array-reduce': 'off',
+		'unicorn/prefer-module': 'off',
+		'unicorn/prefer-object-from-entries': 'off',
+	},
 };
--- scripts/generateSnippetFiles.ts
+++ scripts/generateSnippetFiles.ts
@@ -1,44 +1,44 @@
 import { mkdir, rm } from 'node:fs/promises';
 
 import {
-  JS_FILE_NAME,
-  JS_REACT_FILE_NAME,
-  JS_REACT_SNIPPETS_DIRECTORY,
-  JS_SNIPPETS_DIRECTORY,
-  MD_FILE_NAME,
-  MD_SNIPPETS_DIRECTORY,
-  OUTPUT_DIRECTORY,
-  TS_FILE_NAME,
-  TS_REACT_FILE_NAME,
-  TS_REACT_SNIPPETS_DIRECTORY,
-  TS_SNIPPETS_DIRECTORY,
+	JS_FILE_NAME,
+	JS_REACT_FILE_NAME,
+	JS_REACT_SNIPPETS_DIRECTORY,
+	JS_SNIPPETS_DIRECTORY,
+	MD_FILE_NAME,
+	MD_SNIPPETS_DIRECTORY,
+	OUTPUT_DIRECTORY,
+	TS_FILE_NAME,
+	TS_REACT_FILE_NAME,
+	TS_REACT_SNIPPETS_DIRECTORY,
+	TS_SNIPPETS_DIRECTORY,
 } from './constants';
 import { getSnippetsFromFolder, writeSnippetFile } from './utils';
 
 /** Generate snippets files. */
 const generateSnippetFiles = async (): Promise<void> => {
-  await rm(OUTPUT_DIRECTORY, { recursive: true, force: true });
-  await mkdir(OUTPUT_DIRECTORY);
+	await rm(OUTPUT_DIRECTORY, { recursive: true, force: true });
+	await mkdir(OUTPUT_DIRECTORY);
 
-  const mdSnippetsObject = await getSnippetsFromFolder(MD_SNIPPETS_DIRECTORY);
-  writeSnippetFile(MD_FILE_NAME, mdSnippetsObject);
+	const mdSnippetsObject = await getSnippetsFromFolder(MD_SNIPPETS_DIRECTORY);
+	writeSnippetFile(MD_FILE_NAME, mdSnippetsObject);
 
-  const jsSnippetsObject = await getSnippetsFromFolder(JS_SNIPPETS_DIRECTORY);
-  writeSnippetFile(JS_FILE_NAME, jsSnippetsObject);
+	const jsSnippetsObject = await getSnippetsFromFolder(JS_SNIPPETS_DIRECTORY);
+	writeSnippetFile(JS_FILE_NAME, jsSnippetsObject);
 
-  const jsReactSnippetsObject = await getSnippetsFromFolder(JS_REACT_SNIPPETS_DIRECTORY);
-  writeSnippetFile(JS_REACT_FILE_NAME, { ...jsSnippetsObject, ...jsReactSnippetsObject });
+	const jsReactSnippetsObject = await getSnippetsFromFolder(JS_REACT_SNIPPETS_DIRECTORY);
+	writeSnippetFile(JS_REACT_FILE_NAME, { ...jsSnippetsObject, ...jsReactSnippetsObject });
 
-  const tsSnippetsObject = await getSnippetsFromFolder(TS_SNIPPETS_DIRECTORY);
-  writeSnippetFile(TS_FILE_NAME, { ...jsSnippetsObject, ...tsSnippetsObject });
+	const tsSnippetsObject = await getSnippetsFromFolder(TS_SNIPPETS_DIRECTORY);
+	writeSnippetFile(TS_FILE_NAME, { ...jsSnippetsObject, ...tsSnippetsObject });
 
-  const tsReactSnippetsObject = await getSnippetsFromFolder(TS_REACT_SNIPPETS_DIRECTORY);
-  writeSnippetFile(TS_REACT_FILE_NAME, {
-    ...jsSnippetsObject,
-    ...jsReactSnippetsObject,
-    ...tsSnippetsObject,
-    ...tsReactSnippetsObject,
-  });
+	const tsReactSnippetsObject = await getSnippetsFromFolder(TS_REACT_SNIPPETS_DIRECTORY);
+	writeSnippetFile(TS_REACT_FILE_NAME, {
+		...jsSnippetsObject,
+		...jsReactSnippetsObject,
+		...tsSnippetsObject,
+		...tsReactSnippetsObject,
+	});
 };
 
 generateSnippetFiles();
